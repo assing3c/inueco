a34f3b86f37265979cb31cd2d315ade79d934d36
meowpixel <assing3c@gmail.com>
2026-02-06 11:26:51 +0500

    Тараканов Никита И-107

--- Архитектура информационных систем/lesson4/zadanie 2.py
+++ Архитектура информационных систем/lesson4/zadanie 2.py
@@ -1,17 +1,3 @@
-print ('Hello, World!')
-
-# You know nothing, Jon Snow!
-
-print ('Robert')
-print ('Stannis')
-print ('Renly')
-
-print ('10')
-
-print ('What Is Dead May Never Die')
-
-print (81 / 9)
-
 print (6 + 81)
 
 print (3**5)
